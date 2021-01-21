410814400ee4a204c7b9ee5e9bc9cf6f994cdb2b
Nick Gray <nickgray87@gmail.com>
2021-01-21 20:27:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def list_dwarves(array)
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
-  array.each_with_index do |index, dwarve|
-    puts "#{index} #{dwarve}/n"
+  array.each_with_index do |index = 1, dwarve|
+    puts "#{index}. #{dwarve}/n"
   end
 end
